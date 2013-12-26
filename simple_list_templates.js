09acf2238150976baf50d3df7c73e00f424c7263
William Turner <wktdev@gmail.com>
2013-12-26 14:28:57 -0800

    first commit

---
+++ simple_list_templates.js
@@ -0,0 +1,70 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// library
+
+
+ function template(objName,elementId,loopVal){
+ 	createTemplate = {};
+	    createTemplate.elementId = elementId + "-TEMPLATE";
+	    createTemplate.objName = objName;
+	    createTemplate.loopVal = loopVal;  // display keys,values or both
+	    var  objNameStorage;
+	    var  arrayStorage = [];
+
+	    
+
+	       if(typeof createTemplate.objName === "object"){
+	   
+	       		for(val in objName){
+
+	       			if(createTemplate.loopVal === "key"){
+	       				  objNameStorage = val;
+	       			}
+
+	       			else if(createTemplate.loopVal === "val"){
+	                      objNameStorage = objName[val];
+	       			}
+
+	       			else if(createTemplate.loopVal === "both"){	
+	       				  objNameStorage = val +" "+objName[val];
+
+
+	       			}
+
+          
+          arrayStorage.push('<li>'+objNameStorage+'</li>');
+          console.log(arrayStorage)
+	      document.getElementById(createTemplate.elementId).innerHTML = arrayStorage.join(" ");
+
+					
+	        }
+
+	       }
+
+	       else if(typeof objName === "string"){
+	       objNameStorage = objName;
+	             arrayStorage.push('<li>'+objNameStorage+'</li>');
+          console.log(arrayStorage)
+	      document.getElementById(createTemplate.elementId).innerHTML = arrayStorage.join(" ");
+
+
+	       }
+
+	   
+      
+    
+
+return createTemplate;
+};
+
